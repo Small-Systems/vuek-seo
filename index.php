91bbf4b94dbaf3da38ad08ca49bfc1e48273c766
Luke Archer <info@small-systems.org>
2025-11-07 18:18:52 +0100

    Added missing fields blueprints

--- index.php
+++ index.php
@@ -3,29 +3,31 @@
 use Kirby\Cms\App;
 
 App::plugin('smallsystems/vuek-seo', [
-    'fields' => require __DIR__ . '/src/extensions/fields.php',
-    'translations' => require __DIR__ . '/src/extensions/translations.php',
-    'blueprints' => [
-        'vuek-seo/site' => __DIR__ . '/src/blueprints/tab-site.yml',
-        'vuek-seo/page' => __DIR__ . '/src/blueprints/tab-page.yml',
-    ],
-    'pageMethods' => [
-        'getMetaTitle' => function () {
-            if ($this->intendedTemplate()->name() === 'home') {
-                return site()->title()->value() . ' – ' . $this->metaTitle()->or($this->title())->value();
-            }
-            return $this->metaTitle()->or($this->title() . ' – ' . site()->title())->value();
-        },
-        'getMetaDescription' => function () {
-            return $this->metaDescription()->or($this->writer()->excerpt(140))->or(site()->metaDescription())->value();
-        },
-        'getMetaImage' => function () {
-            return $this->metaImage()->or($this->top()->or(site()->metaImage()))->toFile()->url() ?? '';
-        },
-    ],
-    'siteMethods' => [
-        'getFavicon' => function () {
-            return ['url' => site()->favicon()->toFile()->url() ?? '', 'type' => 'image/' . site()->favicon()->toFile()->extension() ?? ''];
-        },
-    ],
+  'fields' => require __DIR__ . '/src/extensions/fields.php',
+  'translations' => require __DIR__ . '/src/extensions/translations.php',
+  'blueprints' => [
+    'vuek-seo/site' => __DIR__ . '/src/blueprints/tab-site.yml',
+    'vuek-seo/page' => __DIR__ . '/src/blueprints/tab-page.yml',
+    'vuek-seo/meta-image' => __DIR__ . '/src/blueprints/meta-image.yml',
+    'files/favicon' => __DIR__ . '/src/blueprints/favicon.yml'
+  ],
+  'pageMethods' => [
+    'getMetaTitle' => function () {
+      if ($this->intendedTemplate()->name() === 'home') {
+        return site()->title()->value() . ' – ' . $this->metaTitle()->or($this->title())->value();
+      }
+      return $this->metaTitle()->or($this->title() . ' – ' . site()->title())->value();
+    },
+    'getMetaDescription' => function () {
+      return $this->metaDescription()->or($this->writer()->excerpt(140))->or(site()->metaDescription())->value();
+    },
+    'getMetaImage' => function () {
+      return $this->metaImage()->or($this->top()->or(site()->metaImage()))->toFile()->url() ?? '';
+    },
+  ],
+  'siteMethods' => [
+    'getFavicon' => function () {
+      return ['url' => site()->favicon()->toFile()->url() ?? '', 'type' => 'image/' . site()->favicon()->toFile()->extension() ?? ''];
+    },
+  ],
 ]);
--- src/extensions/translations.php
+++ src/extensions/translations.php
@@ -2,15 +2,36 @@
 
 return [
     'en' => [
-        'johannschopplich.serp-preview.label' => 'SERP Preview'
+        'small-systems.vuek-seo.seo-tab.label' => 'Search and Share (SEO)',
+        'small-systems.vuek-seo.seo-preview.label' => 'Preview',
+        'small-systems.vuek-seo.seo-preview.fallback-website-subtitle' => 'Fallback Website Subtitle',
+        'small-systems.vuek-seo.seo-preview.fallback-website-description' => 'Fallback Website Description',
+        'small-systems.vuek-seo.seo-preview.website-icon' => 'Website Icon',
+        'small-systems.vuek-seo.seo-preview.website-image' => ' Fallback Website Image ',
+        'small-systems.vuek-seo.seo-preview.override-meta-title' => 'Override Meta Title',
+        'small-systems.vuek-seo.seo-preview.override-meta-description' => 'Override Meta Description',
+        'small-systems.vuek-seo.seo-preview.override-meta-image' => 'Override Meta Image',
     ],
     'de' => [
-        'johannschopplich.serp-preview.label' => 'SERP-Vorschau'
+        'small-systems.vuek-seo.seo-tab.label' =>  'Suchmaschine und Teilen (SEO)',
+        'small-systems.vuek-seo.seo-preview.label' => 'Vorschau',
+        'small-systems.vuek-seo.seo-preview.fallback-website-subtitle' => 'Ausweich-Website-Untertitel',
+        'small-systems.vuek-seo.seo-preview.fallback-website-description' => 'Ausweich-Website-Beschreibung',
+        'small-systems.vuek-seo.seo-preview.website-icon' => 'Website Symbol',
+        'small-systems.vuek-seo.seo-preview.website-image' => 'Ausweich-Website-Bild',
+        'small-systems.vuek-seo.seo-preview.override-meta-title' => 'Überschreiben Meta Titel',
+        'small-systems.vuek-seo.seo-preview.override-meta-description' => 'Überschreiben Meta Beschreibung',
+        'small-systems.vuek-seo.seo-preview.override-meta-image' => 'Überschreiben Meta Bild',
     ],
     'fr' => [
-        'johannschopplich.serp-preview.label' => 'Aperçu SERP'
-    ],
-    'nl' => [
-        'johannschopplich.serp-preview.label' => 'SERP-voorbeeld'
+        'small-systems.vuek-seo.seo-tab.label' => 'Moteur de recherche et partage (SEO)',
+        'small-systems.vuek-seo.seo-preview.label' => 'Aperçu',
+        'small-systems.vuek-seo.seo-preview.fallback-website-subtitle' => 'Titre du site Web de substitution',
+        'small-systems.vuek-seo.seo-preview.fallback-website-description' => 'Description du site Web de substitution',
+        'small-systems.vuek-seo.seo-preview.website-icon' => 'Icône du site',
+        'small-systems.vuek-seo.seo-preview.website-image' => 'Image de site de substitution',
+        'small-systems.vuek-seo.seo-preview.override-meta-title' => 'Remplacer le sous-titre meta',
+        'small-systems.vuek-seo.seo-preview.override-meta-description' => 'Remplacer la description meta',
+        'small-systems.vuek-seo.seo-preview.override-meta-image' => 'Remplacer l\'image meta',
     ]
 ];
